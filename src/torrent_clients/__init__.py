790540e51aeef8a2d0b37ddf123f1f75a57616d7
mlo-Tek <95163968+mlo-Tek@users.noreply.github.com>
2026-08-29 18:03:31 +0200

    Use Serrva qBittorrent client extensions

--- src/torrent_clients/__init__.py
+++ src/torrent_clients/__init__.py
@@ -1,8 +1,8 @@
 # Upload Assistant © 2025 Audionut & wastaken7 — Licensed under UAPL v1.0
 
 from src.torrent_clients.deluge import DelugeClientMixin
-from src.torrent_clients.qbittorrent import QbittorrentClientMixin
 from src.torrent_clients.rtorrent import RtorrentClientMixin
+from src.torrent_clients.serrva_qbittorrent import SerrvaQbittorrentClientMixin as QbittorrentClientMixin
 from src.torrent_clients.transmission import TransmissionClientMixin
 
 __all__ = [
